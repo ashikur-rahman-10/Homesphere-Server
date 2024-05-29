84c5adb15c673e949af85e120d13f5a3ec8d8eba
Md.Ashikur Rahman <121634529+ashikur-rahman-10@users.noreply.github.com>
2024-05-29 14:52:53 +0600

    users added

--- index.js
+++ index.js
@@ -1,11 +1,100 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("Server is running......")
 })
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a46jnic.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+
+        const userCollections = client.db("MetroMansions").collection("users");
+
+        // Users Api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollections.findOne(query)
+            if (existingUser) {
+                return res.send({ message: "user already exist" })
+            }
+            const result = await usersCollections.insertOne(user);
+            res.send(result)
+        })
+
+        // Get All Users
+        app.get('/users', async (req, res) => {
+            const result = await usersCollections.find().toArray()
+            res.send(result)
+        })
+
+        // Get user by email
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await usersCollections.findOne(filter)
+            res.send(result)
+        })
+
+
+        // update User
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updatedinfo = req.body
+
+            const options = {};
+            // Specify the update to set a value for the plot field
+            const updateDoc = {
+                $set: {
+
+                    phone: updatedinfo.phone,
+                    address: updatedinfo.address
+
+                },
+            };
+            // Update the first document that matches the filter
+            const result = await usersCollections.updateOne(filter, updateDoc, options);
+            if (result.modifiedCount === 1) {
+                res.status(200).json({ acknowledged: true });
+            } else {
+                res.status(500).json({ acknowledged: false, error: "Failed to update user information." });
+            }
+        });
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server is running at port :${port}`);
 })
